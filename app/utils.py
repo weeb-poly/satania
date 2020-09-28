4b6d556d722b22b07beb122791daff8c12f6d185
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2020-09-28 11:33:45 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,22 @@
+from typing import List, TypeVar
+
+from arrow import Arrow
 from discord import Embed
-import html2markdown
+from discord.utils import sleep_until
+from html2markdown import convert as html2md
 
+T = TypeVar('T')
 
-def divide_chunks(arr, n):
+async def sleep_till(a: Arrow) -> None:
+    await sleep_until(a.datetime)
+
+def divide_chunks(arr: List[T], n: int) -> List[List[T]]:
     return [arr[i * n : (i + 1) * n] for i in range((len(arr) + n - 1) // n)]
 
-
-def quickembed(name, desc, begin, end):
+def quickembed(name: str, desc: str, begin: Arrow, end: Arrow) -> Embed:
     return Embed(
         title = name,
-        description = html2markdown.convert(desc)
+        description = html2md(desc)
     ).add_field(
         name = "Start",
         value = begin.to('local').ctime()
